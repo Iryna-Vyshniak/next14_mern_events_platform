67d851a51507996c294b6484efdcc50491b47a4e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-14 22:40:32 +0200

    feat: define headerLinks constant for navigation

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const headerLinks = [
+    {
+        id: 'home-link',
+        label: 'Home',
+        route: '/',
+    },
+    {
+        id: 'create-event-link',
+        label: 'Create Event',
+        route: '/events/create',
+    },
+    {
+        id: 'my-profile-link',
+        label: 'My Profile',
+        route: '/profile',
+    },
+]
