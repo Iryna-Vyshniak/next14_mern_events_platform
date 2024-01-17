6bfbc4b6b85faf21bd3e06983a5dadeca00cba7c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-17 20:55:21 +0200

    feat(ui): implement reusable Label component

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,9 +1,26 @@
-const Label = () => {
-  return (
-    <p className='text-base uppercase font-bold text-primary-500 drop-shadow-[1px_0.5px_0.5px_rgba(0,0,0,1)] tracking-widest'>
-      event<span className='text-white font-bold'>ify</span>
-    </p>
-  );
-};
+"use client"
 
-export default Label;
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+)
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
