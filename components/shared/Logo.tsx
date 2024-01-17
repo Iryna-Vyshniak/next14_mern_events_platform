f48de663e016007a0d235806b96260a0faa4a65e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-17 17:38:14 +0200

    feat(ui): implement reusable logo-label component, update import in Logo component

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import Label from '../ui/label';
+
+import LogoLabel from '../ui/logo-label';
 
 const Logo = ({ src, label }: { src: string; label: boolean }) => {
   return (
     <Link href='/' className='flex-center gap-2 w-38'>
       <Image src={src} alt='Eventify logo' width={38} height={38} quality={100} />
-      {label && <Label />}
+      {label && <LogoLabel />}
     </Link>
   );
 };
---
+++ components/ui/logo-label.tsx
@@ -0,0 +1,9 @@
+const LogoLabel = () => {
+  return (
+    <p className='text-base uppercase font-bold text-primary-500 drop-shadow-[1px_0.5px_0.5px_rgba(0,0,0,1)] tracking-widest'>
+      event<span className='text-white font-bold'>ify</span>
+    </p>
+  );
+};
+
+export default LogoLabel;
