05abcf171e0f364742bf5b9f96525382cc976d54
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-20 00:50:40 +0200

    feat(server): implement createCategory and getAllCategories actions

---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { CreateCategoryParams } from "@/types";
+import { connectToDatabase } from "../database";
+import { handleError } from "../utils";
+import Category from "../database/models/category.model";
+
+// create a new category
+export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
+    try {
+        await connectToDatabase();
+
+        const newCategory = await Category.create({ name: categoryName });
+        console.log("newCategory: ", newCategory);
+
+        return JSON.parse(JSON.stringify(newCategory));
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+
+// get all categories
+export const getAllCategories = async () => {
+    try {
+        await connectToDatabase();
+
+        const categories = await Category.find();
+
+        return JSON.parse(JSON.stringify(categories));
+    } catch (error) {
+        handleError(error)
+    }
+}
