3b63ece58bec5a17187c02997e0314a072426109
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-20 00:54:25 +0200

    style: add styles for datepicker, upd styles for logo-label and select

--- components/ui/logo-label.tsx
+++ components/ui/logo-label.tsx
@@ -1,6 +1,6 @@
 const LogoLabel = () => {
   return (
-    <p className='text-base uppercase font-bold text-primary-500 drop-shadow-[1px_0.5px_0.5px_rgba(0,0,0,1)] tracking-widest'>
+    <p className='logo text-xl uppercase font-bold text-primary-500 drop-shadow-[1px_0.5px_0.5px_rgba(0,0,0,1)] tracking-widest'>
       event<span className='text-white font-bold'>ify</span>
     </p>
   );
