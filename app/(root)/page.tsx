8a78235d206d787c847fe608299fd6135e1a1e8b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-20 17:05:53 +0200

    feat(root): Implement Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <main></main>;
+import Hero from '@/components/shared/Hero';
+import { SearchParamsProps } from '@/types';
+
+export default function Home({ searchParams }: SearchParamsProps) {
+  return (
+    <>
+      <Hero />
+      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+        <h2 className='h2-bold drop-shadow-[1px_1px_1px_rgba(250,250,250,1)]'>
+          Confidence Crafted through <br />
+          Myriad Events
+        </h2>
+        <div className='flex flex-col gap-5 md:flex-row w-full'>Search CategoryFilter</div>
+      </section>
+    </>
+  );
 }
