2d4fe61bb8d21d3255f22ec6e4a11aec9ef044f5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-20 17:01:31 +0200

    feat(server): Implement get one event by id action

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,7 +5,15 @@ import { connectToDatabase } from "../database";
 import { handleError } from "../utils"
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
+import Category from "../database/models/category.model";
 
+const populateEvent = async (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
+
+//  greate event
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
     try {
         await connectToDatabase();
@@ -28,3 +36,18 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
     }
 }
 
+
+// get one event by id
+export async function getEventById(eventId: string) {
+    try {
+        await connectToDatabase()
+
+        const event = await populateEvent(Event.findById(eventId))
+
+        if (!event) throw new Error('Event not found')
+
+        return JSON.parse(JSON.stringify(event))
+    } catch (error) {
+        handleError(error)
+    }
+}
