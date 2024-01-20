57056c60b4d4c5d1a6bece2db1d5da7bad04ccd3
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-20 17:06:49 +0200

    feat(root): Implement page for create event

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { auth } from '@clerk/nextjs';
+
+import EventForm from '@/components/shared/EventForm';
+
+const CreateEvent = () => {
+  // get user id for form
+
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
+  return (
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+        <h3 className='wrapper h3-bold text-center sm:text-left drop-shadow-[1px_1px_2px_rgba(250,250,250,1)]'>
+          Create Event
+        </h3>
+      </section>
+
+      <div className='wrapper my-8'>
+        <EventForm userId={userId} type='Create' />
+      </div>
+    </>
+  );
+};
+
+export default CreateEvent;
